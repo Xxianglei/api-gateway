6cf6964cd87827975beb9b1d288441bd1d5d6c9e
xianglei <xianglei@sefonsoft.com>
2020-04-14 13:49:35 +0800

    修改nacos

--- src/main/java/com/xianglei/ApiGatewayApplication.java
+++ src/main/java/com/xianglei/ApiGatewayApplication.java
@@ -1,20 +1,18 @@
 package com.xianglei;
 
-import com.xianglei.domain.User;
-import com.xianglei.service.UserService;
+
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
-import org.springframework.web.servlet.DispatcherServlet;
 
 @SpringBootApplication
 @EnableZuulProxy
-@EnableEurekaClient
+@EnableFeignClients
+@EnableDiscoveryClient
 @MapperScan("com.xianglei.mapper")
 // 共享session
 @EnableRedisHttpSession
